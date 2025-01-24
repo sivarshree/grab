ee124817d15ac9328cba45dd42bdf4c8d47f3892
sivarshree <sivarshree@gmail.com>
2025-01-24 23:36:43 +0800

    Change default server port from 3000 to 8080

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
